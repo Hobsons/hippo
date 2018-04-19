156349b16f0ee50836755f351bd914828af5559e
Matt Demaray <matt.demaray@hobsons.com>
2018-04-19 11:24:36 -0700

    exit if we lose connection to zk

--- main.py
+++ main.py
@@ -142,9 +142,14 @@ if __name__ == '__main__':
             from kazoo.protocol.states import KazooState
             logging.info('zkstate '+ str(state))
             global leader_election
-            if state in [KazooState.SUSPENDED, KazooState.LOST] and leader_election is not None and not leader_election.lock.is_acquired:
-                logging.info('lost zk connection while waiting to be leader, exiting')
-                leader_election.cancel()
+            if state in [KazooState.SUSPENDED, KazooState.LOST]:
+                if leader_election is not None and not leader_election.lock.is_acquired:
+                    logging.info('lost zk connection while waiting to be leader, exiting')
+                    leader_election.cancel()
+                else:
+                    # we are the leader
+                    exit()
+
 
         zk.add_listener(zk_listen)
         zk.start()
